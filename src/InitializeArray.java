96a2ed6e2eca40b1c1005a929db8c0b684f87b29
sirirak <hotmail22866@gmail.com>
2017-10-10 20:36:23 +0700

    first commit

---
+++ src/InitializeArray.java
@@ -0,0 +1,15 @@
+public class InitializeArray {
+    public static void main(String[] args){
+        int[] intVariable = {5,6};
+        System.out.println(intVariable[1]);
+        System.out.println(intVariable);
+
+        int[] varArray = new int[10];
+        varArray[5] = 10;
+        System.out.println(varArray[5]);
+        System.out.println(varArray[6]);
+
+        
+
+    }
+}
